a1c643dfa3cc24d1cb496ad2b3ed1950ae17115c
Ozan <iphoneozan60@gmail.com>
2026-05-26 07:28:12 +0200

    Creation sous programme ConvertisseurMinutes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿void convertisseurMinutes(int minutes)
+{
+    int Heures = (minutes / 60);
+    int Minutes =  minutes - (Heures * 60);
+    Console.WriteLine($"lheure est {Heures}:{Minutes}" );
+}
+    
+Console.WriteLine("Donner les minutes");
+int m = Convert.ToInt32(Console.ReadLine());
+
+convertisseurMinutes(m);
+
