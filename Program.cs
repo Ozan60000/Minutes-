35574c77f1b2e964b66ae6e24442001d9e809f91
Ozan <iphoneozan60@gmail.com>
2026-05-26 07:47:29 +0200

    Ajout sous-programme AfficherAccueil

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿// Sous-programme ConvertisseurMinutes
+﻿// Sous-programme AfficherAccueil
+// Affiche un message de bienvenue avec la date et l'heure actuelle
+// Aucun paramètre, aucune valeur de retour
+void AfficherAccueil()
+{
+    Console.WriteLine("==============================================");
+    Console.WriteLine("  Bienvenue dans le convertisseur de minutes  ");
+    Console.WriteLine($"  Nous sommes le {DateTime.Now}");
+    Console.WriteLine("==============================================");
+}
+
+// Sous-programme ConvertisseurMinutes
 // Convertit un nombre de minutes au format H:M
 // Paramètre d'entrée :
 //   - minutes : nb de minutes à convertir
@@ -6,11 +17,12 @@
 void convertisseurMinutes(int minutes)
 {
     int Heures = (minutes / 60);
-    int Minutes =  minutes - (Heures * 60);
-    Console.WriteLine($"lheure est {Heures}:{Minutes}" );
+    int Minutes = minutes - (Heures * 60);
+    Console.WriteLine($"lheure est {Heures}:{Minutes}");
 }
-    
+
+// Programme principal
+AfficherAccueil();
 Console.WriteLine("Donner les minutes");
 int m = Convert.ToInt32(Console.ReadLine());
-
 convertisseurMinutes(m);
